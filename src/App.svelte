1104a0039d8a59772fac780268ccc84bc0b683b6
Pyrospower <thepassione7@gmail.com>
2026-05-23 23:05:29 +0200

    chore: init

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<main class="bg-(--background) text-white min-h-screen py-8">
+  <section class="w-full max-w-3xl mx-auto">
+    <h1 class="text-2xl sm:text-3xl font-bold text-center">Warp Planner</h1>
+  </section>
+</main>
